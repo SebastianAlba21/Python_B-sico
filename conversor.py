8722e78d4b160d0d1c8e6e9a542000dc8605c25c
SebastianAlba21 <juan.alba@uao.edu.co>
2022-07-01 17:31:09 -0500

    Conversor realizado con funciones

--- conversor.py
+++ conversor.py
@@ -1,9 +1,27 @@
-pesos = input("Cuantos pesos Colombianos tienes ? : ")
-pesos = float(pesos)
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input("Cuantos pesos " + tipo_pesos + " tienes ? : ")
+    pesos = float(pesos)
+    dolar = pesos / valor_dolar
+    dolar = round(dolar, 2)
+    dolar = str(dolar)
+    print("Tines la cantidad de " + dolar + " Dólares")
 
-valor_rublo = 74.73
-rublos = pesos / valor_rublo
-rublos = round(rublos, 2)
-rublos = str(rublos)
 
-print("Tines la cantidad de " + rublos + " Rublos")
+menu = """
+Bienvenido al conversor de monedas 💰
+
+1- Pesos Colombianos
+2- Pesos Argentinos
+3- Pesos Mexicanos
+Elije una opción: """
+
+opcion = input(menu)
+
+if opcion == '1':
+    conversor("Colombianos", 3875)
+elif opcion == '2':
+    conversor("Argentinos", 65)
+elif opcion == '3':
+    conversor("Mexicanos", 24)
+else:
+    print('Ingresa la opción correcta')
